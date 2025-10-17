afc1ddec3dcf9be55992bbf58b25f73ecf457956
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2025-10-17 13:10:52 +0800

    feat: create question related interfaces

---
+++ src/types/quiz.ts
@@ -0,0 +1,31 @@
+// Question types per requirements
+export type QuestionType = 'text' | 'radio' | 'checkbox';
+
+// Text question - user types their answer
+export interface TextQuestion {
+  id: string;
+  type: 'text';
+  question: string;
+  correctText: string;
+}
+
+// Radio question - single choice
+export interface RadioQuestion {
+  id: string;
+  type: 'radio';
+  question: string;
+  choices: string[];
+  correctIndex: number;
+}
+
+// Checkbox question - multiple choices
+export interface CheckboxQuestion {
+  id: string;
+  type: 'checkbox';
+  question: string;
+  choices: string[];
+  correctIndexes: number[];
+}
+
+// Union of all question types
+export type Question = TextQuestion | RadioQuestion | CheckboxQuestion;
