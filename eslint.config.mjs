8bcfc8b9d1040a11f7fd40476582f12ee4ed0dc4
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2025-10-18 14:20:07 +0800

    chore: update ESLint configuration and add seedrandom dependency for improved randomization support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
@@ -20,6 +23,6 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
